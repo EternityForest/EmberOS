4e7f49888fb71bbe37cb8306582bcc5e906d7260
Daniel Dunn <dannydunn@eternityforest.com>
2020-02-08 05:14:03 -0800

    Various small improvements

--- EmberOS/src/modules/netcfg/filesystem/bin/restart_avahi.sh
+++ EmberOS/src/modules/netcfg/filesystem/bin/restart_avahi.sh
@@ -3,4 +3,7 @@
 #Fix a bug where it sometimes seems to not
 #Show up until you restart the process
 #see https://bugs.launchpad.net/ubuntu/+source/avahi/+bug/624043
-systemctl restart avahi-daemon.service
+systemctl stop avahi-daemon.service
+#Attempt to avoid race conditions with the previous iteration
+sleep 3
+systemctl start avahi-daemon.service
